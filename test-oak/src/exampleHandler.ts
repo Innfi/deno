f714f0c745ca36ade94f7434386b779d8ae96c26
Innfi <innfi.world@gmail.com>
2023-03-10 19:08:36 +0900

    test-oak: need to parse methods / params with types

--- test-oak/src/exampleHandler.ts
+++ test-oak/src/exampleHandler.ts
@@ -2,8 +2,13 @@ import { Reflect } from "https://deno.land/x/reflect_metadata@v0.1.12/mod.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { getQuery } from "https://deno.land/x/oak@v11.1.0/helpers.ts";
 
-import { ClassDeco,MethodDeco,ParamDeco,MetadataEnum,objectList,MethodInfo,ParamInfo, ParamUnit } from "./decorators.ts";
+import { ClassDeco,MethodDeco,ParamDeco,MetadataEnum,objectList,MethodInfo,ParamInfo, ParamUnit, BodyDeco } from "./decorators.ts";
 
+export interface TestBody {
+  userId: number;
+  userName: string;
+  email: string;
+}
 
 @ClassDeco('example')
 export class Example {
@@ -18,11 +23,10 @@ export class Example {
   }
 
   @MethodDeco('post', '/example')
-  setData(
-    @ParamDeco('userId') userId: number,
-    @ParamDeco('userName') userName: string,
-    @ParamDeco('email') email: string
-  ) {
+  setData(@BodyDeco('input') input: TestBody) {
+    console.log(`input: ${JSON.stringify(input)}`);
+    const { userId, userName, email } = input;
+
     this.userId = userId;
     this.userName = userName;
     this.email = email;
@@ -35,11 +39,6 @@ export class Example {
     @ParamDeco('param2') param2: number,
   ): { userName: string; email: string; } | undefined {
     console.log(`getData] called: ${userId}, ${param1}, ${param2}`);
-
-    // return {
-    //   userName: this.userName!,
-    //   email: this.email!, 
-    // };
 
     return {
       userName: 'innfi',
@@ -60,27 +59,25 @@ const naiveRouter = (
   const method = routerParams[0];
   const path = routerParams[1];
 
-  paramUnits.sort((a, b) => a.paramIndex - b.paramIndex);
-  const parameters = paramUnits.map((v) => v.paramName);
-
   if (method === 'get') {
     router[method](path, (context) => {
+      // paramUnits.sort((a, b) => a.paramIndex - b.paramIndex);
+      const parameters = paramUnits.map((v) => v.paramName);
+      console.log(`params: ${JSON.stringify(parameters)}`);
+
       const queries = getQuery(context, { mergeParams: true });
       const methodArgs = parameters.map((v) => queries[v]);
-  
-      // console.log(`methodMetadata] method: ${method}, path: ${path}`);
-      // console.log(`controllerMethod: ${controllerMethod}`);  
-      // console.log(`paramInfo: ${JSON.stringify(parameters)}`);
-      // console.log(`methodArgs: ${JSON.stringify(methodArgs)}`);
+
       context.response.body = instance[controllerMethod](...methodArgs);
     });
 
     return;
-  } else if (method === 'post') {
-    router[method](path, (context) => {
-      //TODO: multiple bodies?
+  } 
+  
+  if (method === 'post') {
+    router[method](path, async (context) => {
+      const body = await context.request.body().value;
 
-      const body = context.request.body();
       context.response.body = instance[controllerMethod](body);
     })
   }
@@ -96,6 +93,7 @@ objectList.forEach((injectorTarget: object) => {
 
   const methodMetadata: MethodInfo = Reflect.getMetadata(MetadataEnum.MethodData, instance);
   const paramMetadata: ParamInfo = Reflect.getMetadata(MetadataEnum.ParamData, instance);
+
   Object.keys(methodMetadata).forEach((v: string) => {
     const controllerMethod = methodMetadata[v];
     const paramUnits: ParamUnit[] = paramMetadata[controllerMethod];
